d0207b96ba7e27bcff35e1fa1d0f9e8f950c0e6d
pub <pubing@go2.cn>
2024-09-23 18:11:22 +0800

    feat: up

--- .deploy.sh
+++ .deploy.sh
@@ -17,10 +17,10 @@ git add -A
 git commit -m 'deploy'
 
 # 如果部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:pbhahaha/pbhahaha.github.io.git master
 
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:pbhahaha/simpler-ui.git master:gh-pages
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 cd -
 
